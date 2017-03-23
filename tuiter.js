415878daf571c49d472f59d4e816be54efda2e21
Jonathan Bradshaw <lavin@posteo.de>
2017-03-22 22:51:34 -0700

    Wrap for loop in a function

--- tuiter.js
+++ tuiter.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
   var $deck = $('.deck');
   $deck.html('');
 
-  var index = streams.home.length - 1;
-  for (index; index >= 0; index--){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    var postedAt = tweet.created_at.toLocaleTimeString();
-    
-    $tweet.text('[' + postedAt + ']' +'@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($deck);
+  function displayTweets(){
+    var index = streams.home.length - 1;
+    for (index; index >= 0; index--){
+      var tweet = streams.home[index];
+      var $tweet = $('<div></div>');
+      var postedAt = tweet.created_at.toLocaleTimeString();
+      
+      $tweet.text('[' + postedAt + ']' +'@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($deck);
+    }
   }
-        
+
+  displayTweets();
 });
