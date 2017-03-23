9bca54d2d339bee2e7946263a27b4adb5a489313
Jonathan Bradshaw <lavin@posteo.de>
2017-03-23 00:03:17 -0700

    Create user object, add parameter to displayTweet function (usr or all)

--- tuiter.js
+++ tuiter.js
@@ -5,19 +5,35 @@ $(document).ready(function(){
   var $feed = $('<div></div>');
   $feed.appendTo($deck);
 
-  function displayTweets(){
+  function displayTweets(context){
     $feed.html('');
 
-    var index = streams.home.length - 1;
+    if (context === 'all') {
+      source = streams.home; // {streams} comes from data_generator.js
+    } else if (context) {
+      source = streams.users[context];
+    }
+
+    var index = source.length - 1;
     for (index; index >= 0; index--){
-      var tweet = streams.home[index];
+      var tweet = source[index];
       var $tweet = $('<div></div>');
       var postedAt = moment(tweet.created_at).format('HH:mm:ss');
-      
+      var $user = $('<a></a>');
+
+      $user.attr({'href': '#', 'data-user': tweet.user, 'class': 'username'});
+      $user.text('<' + tweet.user + '>');
+      $user.appendTo($tweet);
+
       $tweet.text('[' + postedAt + ']  ' +'<' + tweet.user + '>  ' + tweet.message);
       $tweet.appendTo($feed);
     }
+
+    $('.username').on('click', function (e) {
+      e.preventDefault();
+      printTweets($(this).data('user'));
+    });
   }
     
-  setInterval(displayTweets, 1000); 
+  setInterval(displayTweets, 1000, 'all'); 
 });
